43d3f4410425fcfca422d79107a7b6fbd53310ca
remicnrd <remi.cnrd@gmail.com>
2018-03-03 23:43:15 +0100

    save labelEncoder in learning for prediction decoding

--- classifier.py
+++ classifier.py
@@ -1,4 +1,3 @@
-
 import io
 import keras
 import spacy
@@ -20,37 +19,45 @@ class Classifier:
         train = pd.read_csv(trainfile, sep="\t", header=None,
         	names = ["polarity", "aspect_category", "aspect_term", "at_location", "sentence"])
 
-        # We use a tokenizer for text features, and an encoder for categorical features
+        # Create and fit tokenizer with limited word number, save it for prediction
         vocab_size = 3000
         tokenizer = Tokenizer(num_words=vocab_size)
         tokenizer.fit_on_texts(train.sentence)
         with open('tokenizer.pickle', 'wb') as handle:
         	pickle.dump(tokenizer, handle)
-        label_encoder = LabelEncoder()
 
+        # Label encoders for categorical variables
+        label_encoder_category = LabelEncoder()
+        label_encoder_polarity = LabelEncoder()
+
+
+        # Define the BoW vectors using the same matrix
         sentence_tokenized = pd.DataFrame(tokenizer.texts_to_matrix(train.sentence))
         aspect_tokenized = pd.DataFrame(tokenizer.texts_to_matrix(train.aspect_term))
 
-        integer_category = label_encoder.fit_transform(train.aspect_category)
+        # Format categorical variables to one hot
+        integer_category = label_encoder_category.fit_transform(train.aspect_category)
         one_hot_category = pd.DataFrame(to_categorical(integer_category))
-        integer_polarity = label_encoder.fit_transform(train.polarity)
+        integer_polarity = label_encoder_polarity.fit_transform(train.polarity)
         one_hot_polarity = pd.DataFrame(to_categorical(integer_polarity))
 
-        # Define X and y for network input and output
+        # Save the fitted label encoder for prediction decoding
+        with open('label_encoder_polarity.pickle', 'wb') as handle:
+        	pickle.dump(label_encoder_polarity, handle)
+
+        # Define predictors and dependant variable
         X = pd.concat([one_hot_category, aspect_tokenized, sentence_tokenized], axis=1)
         y = one_hot_polarity
 
-        # Define the Neural Network
+        # Create, compile, fit and save the Neural Network
         model = Sequential()
         model.add(Dense(512, input_shape=(6012,)))
         model.add(Activation('relu'))
         model.add(Dense(3))
         model.add(Activation('softmax'))
-
         model.compile(loss='categorical_crossentropy', 
         	optimizer='adam', 
         	metrics=['accuracy'])
-
         model.fit(X, y, epochs=2, verbose=1)
         model.save('model.h5')
 
@@ -64,21 +71,29 @@ class Classifier:
         test = pd.read_csv(datafile, sep="\t", header=None,
         	names = ["polarity", "aspect_category", "aspect_term", "at_location", "sentence"])
 
-        # Data preprocessing
+        # Use fitted tokenizer  to keep the same BoW vectors
         with open('tokenizer.pickle', 'rb') as handle:
         	tokenizer = pickle.load(handle)
+
+        # Use fitted label_Encoder to decode predictions
+        with open('label_encoder_polarity.pickle', 'rb') as handle:
+            label_encoder_polarity = pickle.load(handle)
+
         label_encoder = LabelEncoder()
 
+        # Define the BoW vectors using the same matrix
         sentence_tokenized = pd.DataFrame(tokenizer.texts_to_matrix(test.sentence))
         aspect_tokenized = pd.DataFrame(tokenizer.texts_to_matrix(test.aspect_term))
         
+        # Format categorical variables to one hot
         integer_category = label_encoder.fit_transform(test.aspect_category)
         one_hot_category = pd.DataFrame(to_categorical(integer_category))
 
         X = pd.concat([one_hot_category, aspect_tokenized, sentence_tokenized], axis=1)
 
+        # Load the model weights and architecture, predict for new data
         model = load_model('model.h5')
         predictions = model.predict_classes(X,)
-        return label_encoder.inverse_transform(predictions)
+        return label_encoder_polarity.inverse_transform(predictions)
 
 
