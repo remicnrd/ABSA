d52d8a5ef7e88a52167766c735251acdacd043a6
remicnrd <remi.cnrd@gmail.com>
2018-03-03 20:46:39 +0100

    first commit

---
+++ classifier.py
@@ -0,0 +1,21 @@
+
+
+
+class Classifier:
+    """The Classifier"""
+
+
+    #############################################
+    def train(self, trainfile):
+        """Trains the classifier model on the training set stored in file trainfile"""
+
+
+    def predict(self, datafile):
+        """Predicts class labels for the input instances in file 'datafile'
+        Returns the list of predicted labels
+        """
+
+
+
+
+
